799d3a38217123d52227bc953f7d53763795efe3
StevenChiu2018 <35871453+StevenChiu2018@users.noreply.github.com>
2020-07-04 00:44:29 +0800

    Score class done

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 LENGTH = 100
 WIDTH = 50
 POINT = 5
+LIFE = 5
+BALLAMOUNT = 20
 
 class Ball:
     def __init__(self, start_point):
@@ -9,8 +11,35 @@ class Ball:
     def down(self):
         if self.y < LENGTH:
             self.y = self.y + 1
-    def terminate(self):
+    def is_terminate(self):
         if self.y == LENGTH:
             return True
         else:
             return False
+
+class Score:
+    def __init__(self, player):
+        self.life = LIFE
+        self.point = 0
+        self.ball = BALLAMOUNT
+        self.name = player
+    def is_end(self):
+        if self.life <= 0 or self.ball <= 0:
+            return True
+        else:
+            return False
+    def show_score(self):
+        if self.point >= 90:
+            print(self.name + " ， 您好棒！")
+        elif self.point >= 75:
+            print(self.name + " ， 不錯唷！")
+        elif self.point >= 60:
+            print(self.name + " ， 還可以！")
+        else:
+            print(self.name + " ， 在努力練習！")
+    def add_point(self):
+        self.point = self.point + POINT
+    def deduct_life(self):
+        self.life = self.life - 1
+    def drop_ball(self):
+        self.ball = self.ball - 1
